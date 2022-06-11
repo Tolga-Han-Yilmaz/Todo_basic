b04d4f47511fc3125c8f2a4c06f0985688984ec6
Thy <06thy40@gmail.com>
2022-06-11 15:54:46 +0300

    added delete

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
+// Selectors
 const btn = document.getElementById("todo-button")
 const todoInput = document.getElementById("todo-input")
 const todoUI = document.getElementById("todo-ul")
 
+// başlangiçta input'A focus olsun
 window.onload = function(){
     todoInput.focus()
 }
 
 btn.addEventListener("click",(e)=>{
-todoUI.innerHTML += `
-<li>
-    <i class="fa fa-check"></i>
-    <p>${todoInput.value}</p>
-    <i class="fa fa-trash"></i>
-</li>`
-todoInput.value=""
-})
+    if(!todoInput.value){
+        alert("Please enter your todo")
+    }else{
+
+        todoUI.innerHTML += `
+        <li>
+        <i class="fa fa-check"></i>
+        <p>${todoInput.value}</p>
+        <i class="fa fa-trash"></i>
+        </li>`
+        todoInput.value=""
+    }
+    })
 
 todoInput.addEventListener("keydown",(e)=>{
     // inputta iken enter'a tıklayınca btn.click() fonk çağır.
     if(e.key==="Enter"){
         btn.click()
+    }
+})
+
+// delete ve enter tuşuna basilmasi ile add butonunun click fonk çağırılması
+todoUI.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("fa-trash")){
+        e.target.parentElement.remove()
+    }
+    if(e.target.classList.contains("fa-check")){
+        if(e.target.parentElement.classList.contains("checked")){
+            e.target.parentElement.classList.remove("checked")
+        }else{
+            e.target.parentElement.classList.add("checked")
+        }
     }
 })
